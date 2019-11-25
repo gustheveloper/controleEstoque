781a7d4a42fadf8c7f029604c496093ba9d0761e
Gustavo Corrêa <gustheveloper@gmail.com>
2019-11-25 13:05:19 -0200

    Criando tabelas Users e Products pelo laravel

--- database/migrations/2019_11_25_145214_create_products.php
+++ database/migrations/2019_11_25_145214_create_products.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProducts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('img profile');
-            $table->string('description');
-            $table->Integer('active');
-            $table->rememberToken();
+            $table->string('description', 300);
+            $table->integer('quantity', 10);
+            $table->float('price', 10, 2);
+            $table->foreign('user_id')->references ('id')->on('users');                                                                            
             $table->timestamps();
         });
     }
@@ -34,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
